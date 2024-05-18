830bac924ab1fd2231526a3e245cc255de713ca4
haseebahmad9418 <haseebahmad9418@gmail.com>
2024-05-18 17:29:57 +0500

    landing page done

---
+++ creative_readers_publications/src/components/Ovalbottom.tsx
@@ -0,0 +1,12 @@
+function Ovalbottom(){
+    return(
+        <>
+            <div className="oval-bottom-container">
+                <div className="oval-bottom">
+                    <h2 className="text-white text-center">Powered By Embed.me</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Ovalbottom;
---
+++ creative_readers_publications/src/components/Ovaltop.tsx
@@ -0,0 +1,10 @@
+function Ovaltop(){
+    return(
+        <>
+            <div className="oval-top-container">
+            <div className="oval-top"></div>
+</div>
+        </>
+    )
+}
+export default Ovaltop;
